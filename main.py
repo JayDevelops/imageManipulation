041f8f9138718142be07b4f9db56207f162cb61a
JayDevelops <jaymations1234@gmail.com>
2019-11-17 23:53:48 -0800

    The images cycle through, hell yessss

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 # Change the image when this function is called when the button is pressed
 def changeImage():
     # We are grabbing a random imageListArray to the imageSet variable
-    imageListArray = ["cat1.png", "spaceGuy.png", "squirrel-coffee.png", "vigo.png"]
+    imageListArray = [img1, img2, img3]
     randomNumber = random.randint(0, 3)
 
     # Get the random number equal to the array here
@@ -13,13 +13,20 @@ def changeImage():
 
 # Make a new root window here
 root = tkinter.Tk()
-root.geometry("800x500")
+root.geometry("1200x1080")
 root.columnconfigure(0, minsize=100)
 root.columnconfigure(1, minsize=200)
 
 
+# DEFINE ALL IMAGES AS GLOBALS HERE
+img1 = tkinter.PhotoImage(file="spaceGuy.png")
+img2 = tkinter.PhotoImage(file="vigo.png")
+img3 = tkinter.PhotoImage(file="squirrel-coffee.png")
+img4 = tkinter.PhotoImage(file="cat1.png")
+
+
 # Make a static label for the images to change here
-imageLabel = tkinter.Label(root, text="images", img)
+imageLabel = tkinter.Label(root, text="images", image=img1)
 imageLabel.grid(row=10, rowspan=3, column=3)
 
 
